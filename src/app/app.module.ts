4f247e7e8ef9e13811f02322e9979c07f9c492a3
Ravi Kiran <yuvakiran2009@gmail.com>
2017-10-27 23:58:51 +0530

    Added pokemon app and server rendering capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { routesModule } from './app.routes';
 
 import { AppComponent } from './app.component';
 
+import { PokemonService } from './pokemon.service';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonDetailsComponent
   ],
   imports: [
-    BrowserModule
+    // Add .withServerTransition() to support Universal rendering.
+    // The application ID can be any identifier which is unique on
+    // the page.
+    BrowserModule.withServerTransition({appId: 'my-app'}), routesModule, HttpModule
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
